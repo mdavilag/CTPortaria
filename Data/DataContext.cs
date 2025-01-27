0f49db064a94ecd730b957d7651fd61cf69d601d
Matheus Davila <mat.97@hotmail.com>
2025-01-27 16:53:13 -0300

    Added DataContext with DbSets

---
+++ Data/DataContext.cs
@@ -0,0 +1,23 @@
+﻿using CTPortaria.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace CTPortaria.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+
+        }
+        public DbSet<EmployeeModel> Employees { get; set; }
+        public DbSet<GateLogModel> GateLogs { get; set; }
+        public DbSet<PackageModel> Packages { get; set; }
+        public DbSet<VehicleModel> Vehicles { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            //modelBuilder.ApplyConfiguration(new UserMap());
+
+        }
+
+    }
+}
