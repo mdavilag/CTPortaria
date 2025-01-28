79e817c5905572a92565a245103f32129b6afad4
Matheus Davila <mat.97@hotmail.com>
2025-01-28 19:31:39 -0300

    Created IEmployeeService and ServiceDTO

---
+++ DTOs/EmployeeServiceDTO.cs
@@ -0,0 +1,10 @@
+﻿namespace CTPortaria.DTOs
+{
+    public class EmployeeServiceDTO
+    {
+        public string Name { get; set; }
+        public string Cpf { get; set; }
+        public string JobRole { get; set; }
+        public bool IsActive { get; set; }
+    }
+}
--- Services/Interfaces/IEmployeeService.cs
+++ Services/Interfaces/IEmployeeService.cs
@@ -1,10 +1,18 @@
 ﻿using CTPortaria.DTOs;
+using CTPortaria.Entities;
 
 namespace CTPortaria.Services.Interfaces
 {
 
     public interface IEmployeeService
     {
+        Task<EmployeeServiceDTO> GetByNameAsync(string name);
+        Task<List<EmployeeServiceDTO>> GetAllAsync();
+        Task<EmployeeServiceDTO> GetByIdAsync(int id);
         Task<EmployeeCreateDto> CreateAsync(EmployeeCreateDto employeeCreateDto);
+        Task<EmployeeServiceDTO> UpdateAsync(EmployeeServiceDTO employeeServiceDTO);
+        Task<bool> DeleteByIdAsync(int id);
+        Task<bool> ExistsById(int id);
+
     }
 }
