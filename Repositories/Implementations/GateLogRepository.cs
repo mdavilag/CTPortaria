f3d79373d1514166cd89b294e17e2a543cd0c23a
Matheus Davila <mat.97@hotmail.com>
2025-03-17 21:08:28 -0300

    Create GateLogRepository
    Methods GetAllAsync and GetAllnsideAsync

---
+++ Repositories/Implementations/GateLogRepository.cs
@@ -0,0 +1,55 @@
+﻿using CTPortaria.Data;
+using CTPortaria.Entities;
+using CTPortaria.Repositories.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace CTPortaria.Repositories.Implementations
+{
+    public class GateLogRepository : IGateLogRepository
+    {
+        private readonly DataContext _context;
+
+        public GateLogRepository(DataContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<List<GateLogModel>> GetAllAsync()
+        {
+            return await _context.GateLogs.ToListAsync();
+        }
+
+        public async Task<List<GateLogModel>> GetAllInsideAsync()
+        {
+            return await _context.GateLogs
+                .Where(x => x.LeavedAt == null)
+                .Include(x=>x.Employee)
+                .ToListAsync();
+        }
+
+        public async Task<GateLogModel> GetByIdAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<List<GateLogModel>> GetByDayAsync(DateTime date)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<List<GateLogModel>> GetByDateTimeAsync(DateTime initDate, DateTime endDate)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<List<GateLogModel>> GetByEmployeeAsync(EmployeeModel employee)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<List<GateLogModel>> GetByVisitorIdentity(string visitorName)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- Repositories/Interfaces/IGateLogRepository.cs
+++ Repositories/Interfaces/IGateLogRepository.cs
@@ -5,11 +5,12 @@ namespace CTPortaria.Repositories.Interfaces
     public interface IGateLogRepository
     {
         Task<List<GateLogModel>> GetAllAsync();
+        Task<List<GateLogModel>> GetAllInsideAsync();
         Task<GateLogModel> GetByIdAsync(int id);
         Task<List<GateLogModel>> GetByDayAsync(DateTime date);
         Task<List<GateLogModel>> GetByDateTimeAsync(DateTime initDate, DateTime endDate);
         Task<List<GateLogModel>> GetByEmployeeAsync(EmployeeModel employee);
-        Task<List<GateLogModel>> GetByVisitorNameAsync(string visitorName);
+        Task<List<GateLogModel>> GetByVisitorIdentity(string visitorName);
 
         // Create
         // Update
