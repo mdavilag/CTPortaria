dbeac0a59ed899acfb870d191a7fb50c125dc894
Matheus Davila <mat.97@hotmail.com>
2025-03-22 19:15:19 -0300

    Created GatelogRepository and Service dependency injections

--- Program.cs
+++ Program.cs
@@ -19,8 +19,11 @@ builder.Services.AddDbContext<DataContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 builder.Services.AddScoped<DataContext>();
+// Dependency Injections
 builder.Services.AddScoped<IEmployeeRepository,EmployeeRepository>();
 builder.Services.AddScoped<IEmployeeService, EmployeeService>();
+builder.Services.AddScoped<IGateLogRepository, GateLogRepository>();
+builder.Services.AddScoped<IGateLogService, GateLogService>();
 builder.Services.AddTransient<IEmployeeValidator, EmployeeValidator>();
 
 var app = builder.Build();
--- Services/Implementations/GateLogService.cs
+++ Services/Implementations/GateLogService.cs
@@ -1,9 +1,65 @@
-﻿using CTPortaria.Services.Interfaces;
+﻿using AutoMapper;
+using CTPortaria.Data;
+using CTPortaria.DTOs;
+using CTPortaria.Repositories.Interfaces;
+using CTPortaria.Services.Interfaces;
+using Microsoft.EntityFrameworkCore;
 
 namespace CTPortaria.Services.Implementations
 {
     public class GateLogService : IGateLogService
     {
+        private readonly IGateLogRepository _repository;
+        private readonly IMapper _mapper;
 
+        public GateLogService(IGateLogRepository repository, IMapper mapper)
+        {
+            _repository = repository;
+            _mapper = mapper;
+        }
+        public async Task<List<GateLogServiceDTO>> GetAllAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<List<GateLogServiceDTO>> GetallInsideAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<GateLogServiceDTO> GetByIdAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<List<GateLogServiceDTO>> GetByDayAsync(DateTime date)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<List<GateLogServiceDTO>> GetByDateTimeAsync(DateTime initDate, DateTime endDate)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<List<GateLogServiceDTO>> GetByEmployeeAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<List<GateLogServiceDTO>> GetByVisitorCpfAsync(string visitorCpf)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<GateLogServiceDTO> RegisterExitAsync(int gateLogId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<bool> DeleteByIdAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
