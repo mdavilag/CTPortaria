4325c01981392299ba8611b2a9af47b48f868d59
Matheus Davila <mat.97@hotmail.com>
2025-03-21 21:34:51 -0300

    Created GateLogServiceDTO

--- DTOs/GateLogServiceDTO.cs
+++ DTOs/GateLogServiceDTO.cs
@@ -1,7 +1,19 @@
-﻿namespace CTPortaria.DTOs
+﻿using Newtonsoft.Json;
+
+namespace CTPortaria.DTOs
 {
     public class GateLogServiceDTO
     {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Cpf { get; set; }
 
+        public string PersonType { get; set; } // employee or visitor (change to Enum)
+
+        public DateTime EnteredAt { get; set; }
+        public DateTime? LeavedAt { get; set; }
+
+        public string RegisteredBy { get; set; }
+        public string Description { get; set; }
     }
 }
