963367d9fd274776ee846f694a82af3928e1fd09
Matheus Davila <mat.97@hotmail.com>
2025-03-22 19:09:52 -0300

    Created IGateLogService

---
+++ Services/Implementations/GateLogService.cs
@@ -0,0 +1,9 @@
+﻿using CTPortaria.Services.Interfaces;
+
+namespace CTPortaria.Services.Implementations
+{
+    public class GateLogService : IGateLogService
+    {
+
+    }
+}
--- Services/Interfaces/IGateLogService.cs
+++ Services/Interfaces/IGateLogService.cs
@@ -1,7 +1,28 @@
-﻿namespace CTPortaria.Services.Interfaces
+﻿using CTPortaria.DTOs;
+using CTPortaria.Entities;
+
+namespace CTPortaria.Services.Interfaces
 {
     public interface IGateLogService
     {
+        Task<List<GateLogServiceDTO>> GetAllAsync();
+        Task<List<GateLogServiceDTO>> GetallInsideAsync();
 
+        Task<GateLogServiceDTO> GetByIdAsync(int id);
+        Task<List<GateLogServiceDTO>> GetByDayAsync(DateTime date);
+        Task<List<GateLogServiceDTO>> GetByDateTimeAsync(DateTime initDate, DateTime endDate);
+        Task<List<GateLogServiceDTO>> GetByEmployeeAsync(int id);
+        Task<List<GateLogServiceDTO>> GetByVisitorCpfAsync(string visitorCpf);
+
+        // Create
+        // Task<GateLogServiceDTO> CreateAsync(GateLogModel gateLogToCreate) -> Falta criar create/updateDTO
+
+        // Update
+        // Task<GateLogServiceDTO> UpdateAsync(); -> Falta criar create/updateDTO
+        Task<GateLogServiceDTO> RegisterExitAsync(int gateLogId);
+
+        // Delete
+
+        Task<bool> DeleteByIdAsync(int id);
     }
 }
