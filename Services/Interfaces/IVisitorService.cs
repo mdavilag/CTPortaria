dd5f9b82b73f22e5ca1e705fb73495e7270f5cc9
Matheus Davila <mat.97@hotmail.com>
2025-03-30 20:35:31 -0300

    Created IVisitorService interface

--- Services/Interfaces/IVisitorService.cs
+++ Services/Interfaces/IVisitorService.cs
@@ -1,7 +1,17 @@
-﻿namespace CTPortaria.Services.Interfaces
+﻿using CTPortaria.DTOs;
+using CTPortaria.Entities;
+
+namespace CTPortaria.Services.Interfaces
 {
     public interface IVisitorService
     {
-
+        Task<VisitorServiceDTO> GetByNameAsync(string name);
+        Task<List<VisitorServiceDTO>> GetAllAsync();
+        Task<VisitorServiceDTO> GetByIdAsync(int id);
+        Task<VisitorServiceDTO> CreateAsync(VisitorCreateDTO visitorToCreate);
+        Task<VisitorServiceDTO> UpdateAsync(VisitorCreateDTO visitorToUpdate);
+        Task<bool> DeleteByIdAsync(int id);
+        Task<bool> ExistsById(int id);
+        Task<bool> ExistsByCpf(string cpf);
     }
 }
