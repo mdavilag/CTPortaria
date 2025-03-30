6db4e3ca60a26c53ad50ae2b66bb354f4b0422c7
Matheus Davila <mat.97@hotmail.com>
2025-03-30 20:02:24 -0300

    Created IVisitorRepository

---
+++ Repositories/Interfaces/IVisitorRepository.cs
@@ -0,0 +1,16 @@
+﻿using CTPortaria.Entities;
+
+namespace CTPortaria.Repositories.Interfaces
+{
+    public interface IVisitorRepository
+    {
+        Task<VisitorModel> GetByNameAsync(string name);
+        Task<List<VisitorModel>> GetAllAsync();
+        Task<VisitorModel> GetByIdAsync(int id);
+        Task<VisitorModel> CreateAsync(VisitorModel visitorToCreate);
+        Task<VisitorModel> UpdateAsync(VisitorModel visitorToUpdate);
+        Task<bool> DeleteByIdAsync(int id);
+        Task<bool> ExistsById(int id);
+        Task<bool> ExistsByCpf(string cpf);
+    }
+}
